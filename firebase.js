2645e3be075f64414941ff1077d16074f9a1f556
KatarzynaKopcinska <pasjapoprostu746@gmail.com>
2025-12-29 14:59:56 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,19 +5,18 @@
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-storage-compat.js"></script>
 
 <script>
-  const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCJug-WWwnAjTbyPJBf5rgLdZD2oKWg_e4",
   authDomain: "pasjapoprostu-386fb.firebaseapp.com",
   projectId: "pasjapoprostu-386fb",
-  storageBucket: "pasjapoprostu-386fb.firebasestorage.app",
+  storageBucket: "pasjapoprostu-386fb.appspot.com",
   messagingSenderId: "1025672670668",
   appId: "1:1025672670668:web:f2f114204eab7d32aa8169",
   measurementId: "G-23E3MK83SS"
 };
-
-  firebase.initializeApp(firebaseConfig);
-  const auth = firebase.auth();
-  const db = firebase.firestore();
-  const storage = firebase.storage();
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
 </script>
 
