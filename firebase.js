6a9af77f397231dd691a81bfdfc91b030e9caf53
KatarzynaKopcinska <pasjapoprostu746@gmail.com>
2025-12-29 15:40:44 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,6 @@
-<!-- firebase.js -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-storage-compat.js"></script>
+// firebase.js
 
-<script>
+// Konfiguracja Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCJug-WWwnAjTbyPJBf5rgLdZD2oKWg_e4",
   authDomain: "pasjapoprostu-386fb.firebaseapp.com",
@@ -14,9 +10,9 @@ const firebaseConfig = {
   appId: "1:1025672670668:web:f2f114204eab7d32aa8169",
   measurementId: "G-23E3MK83SS"
 };
+
+// Inicjalizacja Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
 const storage = firebase.storage();
-</script>
-
